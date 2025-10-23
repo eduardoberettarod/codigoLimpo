b317cbe2376ae6f14a00e567001e25cc3844ab90
eduardoberettarod <eduardo.berettarod@gmail.com>
2025-10-23 20:12:54 -0300

    edaurdo

--- eduardo/organizado/js/script.js
+++ eduardo/organizado/js/script.js
@@ -1,3 +1,5 @@
+//função que puxa o fundo animado para a tela inicial
+
 particlesJS.load('particles-js', '../js/particlesjs-config.json', function () {
     console.log('Particles.js carregado com sucesso 🎉');
 });
